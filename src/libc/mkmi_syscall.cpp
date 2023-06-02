ff6b4a206fe870968980bab6770061e7453ea268
Filippo Mutta <filippo.mutta@gmail.com>
2023-06-02 13:09:57 +0200

    Added syscall method by using the so-called instruction in x86_64

--- src/libc/mkmi_syscall.cpp
+++ src/libc/mkmi_syscall.cpp
@@ -1,6 +1,6 @@
 #include <mkmi_syscall.h>
 
-inline size_t __x64_syscall(size_t syscallNum, size_t arg1, size_t arg2, size_t arg3, size_t arg4, size_t arg5) {
+inline size_t __x64_int_syscall(size_t syscallNum, size_t arg1, size_t arg2, size_t arg3, size_t arg4, size_t arg5) {
 	size_t ret;
 	asm volatile("mov %1, %%rdi\n\t"
 		     "mov %2, %%rsi\n\t"
@@ -18,6 +18,23 @@ inline size_t __x64_syscall(size_t syscallNum, size_t arg1, size_t arg2, size_t 
 	return syscallNum;
 }
 
+inline size_t __x64_syscall(size_t syscallNum, size_t arg1, size_t arg2, size_t arg3, size_t arg4, size_t arg5) {
+	size_t ret;
+	asm volatile("mov %1, %%rdi\n\t"
+		     "mov %2, %%rsi\n\t"
+		     "mov %3, %%rdx\n\t"
+		     "mov %4, %%rcx\n\t"
+		     "mov %5, %%r8\n\t"
+		     "mov %6, %%r9\n\t"
+		     "syscall\n\t"
+		     "mov %%rax, %0"
+		     : "=r"(ret)
+		     : "r"(syscallNum), "r"(arg1), "r"(arg2), "r"(arg3), "r"(arg4), "r"(arg5)
+		     : "memory", "cc", "rax", "rdi", "rdi", "rdx", "rcx", "r8", "r9");
+
+
+	return syscallNum;
+}
 
 
 size_t Syscall(size_t syscallNum, size_t arg1, size_t arg2, size_t arg3, size_t arg4, size_t arg5) {
