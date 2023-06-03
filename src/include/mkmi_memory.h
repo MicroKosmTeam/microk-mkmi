7b7c5eee984eaf9df07b8ef07e0d11e923058403
Filippo Mutta <filippo.mutta@gmail.com>
2023-06-03 15:55:48 +0200

    Added memory info struct.

--- src/include/mkmi_memory.h
+++ src/include/mkmi_memory.h
@@ -6,6 +6,13 @@
 extern "C" {
 #endif
 
+typedef struct {
+	size_t MemoryTotal;
+	size_t MemoryFree;
+	size_t MemoryReserved;
+	size_t MemoryBuffers;
+}__attribute((packed)) MemoryInfo;
+
 extern void (*Memcpy)(void *dest, void *src, size_t n);
 extern void (*Memset)(void *start, uint8_t value, uint64_t num);
 extern int (*Memcmp)(const void* buf1, const void* buf2, size_t count);
