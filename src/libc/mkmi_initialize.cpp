586f23429379e2a2911fb6a5da280c1ccda15ff9
Filippo Mutta <filippo.mutta@gmail.com>
2023-08-17 14:41:50 +0200

    Reduced default heap size to 512kb

--- src/libc/mkmi_initialize.cpp
+++ src/libc/mkmi_initialize.cpp
@@ -15,9 +15,9 @@ extern "C" void __mkmi_init() {
 
 	MKMI_Printf("Initializing the heap...\r\n");
 
-	/* 256GB as base, 32MB in size. Should be good for now */
+	/* 256GB as base, 512kb in size. Should be good for now */
 	const uintptr_t heapBase = (0x100000000);
-	const size_t heapLength = (2 * 1024 * 1024);
+	const size_t heapLength = (512 * 1024);
 	MKMI_InitializeHeap(heapBase, heapLength);
 
 	MKMI_Printf("Initialization complete.\r\n");
