3b122d155f9e97a8e6b8b7cacefd9504825d57d0
Filippo Mutta <filippo.mutta@gmail.com>
2023-11-15 15:33:16 +0100

    Updated MKMI to use the new message system.

--- src/libc/mkmi_syscall.cpp
+++ src/libc/mkmi_syscall.cpp
@@ -1,7 +1,9 @@
 #include <mkmi_syscall.h>
 
-inline size_t __x64_int_syscall(size_t syscallNum, size_t arg1, size_t arg2, size_t arg3, size_t arg4, size_t arg5, size_t arg6) {
-	asm volatile("mov %0, %%rax\n\t"
+inline __attribute__((always_inline))
+size_t __x64_int_syscall(size_t syscallNum, size_t arg1, size_t arg2, size_t arg3, size_t arg4, size_t arg5, size_t arg6) {
+	asm volatile(
+		     "mov %0, %%rax\n\t"
 		     "mov %1, %%rdi\n\t"
 		     "mov %2, %%rsi\n\t"
 		     "mov %3, %%rdx\n\t"
@@ -9,14 +11,16 @@ inline size_t __x64_int_syscall(size_t syscallNum, size_t arg1, size_t arg2, siz
 		     "mov %5, %%r9\n\t"
 		     "mov %6, %%r10\n\t"
 		     "int $0xFE\n\t"
-		     : 
+		     "mov %%rax, %0\n\t"
+		     :
 		     : "r"(syscallNum), "r"(arg1), "r"(arg2), "r"(arg3), "r"(arg4), "r"(arg5), "r"(arg6)
-		     : "memory", "cc", "rax", "rdi", "rsi", "rdx", "rcx", "r8", "r9");
+		     : "memory", "cc", "rax", "rdi", "rsi", "rdx", "r8", "r9", "r10");
 
 	return syscallNum;
 }
 
-inline size_t __x64_syscall(size_t syscallNum, size_t arg1, size_t arg2, size_t arg3, size_t arg4, size_t arg5, size_t arg6) {
+inline __attribute__((always_inline))
+size_t __x64_syscall(size_t syscallNum, size_t arg1, size_t arg2, size_t arg3, size_t arg4, size_t arg5, size_t arg6) {
 	asm volatile(
 		     "mov %0, %%rax\n\t"
 		     "mov %1, %%rdi\n\t"
